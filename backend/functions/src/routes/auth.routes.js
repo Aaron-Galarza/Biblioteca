b48adc5bdc85296cbb170d0aa983da57b4fcea0c
nazabe <nazarenobeauvais@gmail.com>
2025-11-27 16:57:18 -0300

    build(frontend): login works well

--- backend/functions/src/routes/auth.routes.js
+++ backend/functions/src/routes/auth.routes.js
@@ -11,15 +11,5 @@ router.post("/login", login);
 // LEE JWT DESDE COOKIE
 router.get("/profile", authRequired, profile);
 
-// Logout borrando cookie
-router.post("/logout", (req, res) => {
-  res.clearCookie("auth_token", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    path: "/",
-  });
-  res.json({ msg: "Logout exitoso" });
-});
 
 export default router;
